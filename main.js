2d09142ae8920231fa9c382fd0c6ebb34b65dfe8
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-07-31 14:01:11 +0200

    optimization splash behaviour

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ barba.init({
       beforeEnter() {
         home.classList.add("active");
         home.href = "./index.html";
-        projects.href = "../projects/index.html";
-        contact.href = "../contact/index.html";
+        projects.href = "../projects/";
+        contact.href = "../contact/";
       },
       beforeLeave() {
         home.classList.remove("active");
@@ -23,7 +23,7 @@ barba.init({
         projects.classList.add("active");
         home.href = "../index.html";
         projects.href = "./index.html";
-        contact.href = "../contact/index.html";
+        contact.href = "../contact/";
       },
       beforeLeave() {
         projects.classList.remove("active");
@@ -34,7 +34,7 @@ barba.init({
       beforeEnter() {
         contact.classList.add("active");
         contact.href = "../index.html";
-        projects.href = "../projects/index.html";
+        projects.href = "../projects/";
         contact.href = "./index.html";
       },
       beforeLeave() {
