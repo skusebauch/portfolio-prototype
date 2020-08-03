74dd63f81732c94794010478c5bbff02e872145e
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-03 13:28:34 +0200

    small changes before deploy

--- main.js
+++ main.js
@@ -68,3 +68,21 @@ barba.init({
     },
   ],
 });
+
+$(document).ready(function () {
+  const menuBtn = $("a");
+
+  menuBtn.click(() => {
+    setTimeout(() => {
+      removeHash();
+    }, 5);
+  });
+
+  function removeHash() {
+    history.replaceState(
+      "",
+      document.title,
+      window.location.origin + window.location.pathname + window.location.search
+    );
+  }
+});
